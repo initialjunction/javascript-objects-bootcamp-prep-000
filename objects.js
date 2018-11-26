23b0992600dcaef1ad271141d2e4e482f5e78e17
Joanna Tomassoni <jctomassoni@gmail.com>
2018-11-26 03:12:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = {
   artist: "song title"
 }
 
-function updatePlaylist (obj, key, value) {
-  
+function updatePlaylist (playlist, artistName, songTitle) {
+  playlist.push({artistName: songTitle});
 }
